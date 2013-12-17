1a861cdd1d10b6e6e4359f842c71aca4cdb025e6
Nikolay Zalutskiy <pacemkr@gmail.com>
2013-12-17 16:33:17 -0500

    Add decrypt function to interface.

--- scrypt.ml
+++ scrypt.ml
@@ -1,5 +1,10 @@
 external scryptenc_buf : string -> string -> int -> float -> float -> string = "scryptenc_buf_stub"
+external scryptdec_buf : string -> string -> int -> float -> float -> string = "scryptdec_buf_stub"
 
+(* Default values for optional arguments are chosen to match the scrypt command line utility. *)
 
 let encrypt ?(maxmem=0) ?(maxmemfrac=0.125) ?(maxtime=5.0) data passwd =
   scryptenc_buf data passwd maxmem maxmemfrac maxtime
+
+let decrypt ?(maxmem=0) ?(maxmemfrac=0.5) ?(maxtime=300.0) data passwd =
+  scryptdec_buf data passwd maxmem maxmemfrac maxtime
--- scrypt.mli
+++ scrypt.mli
@@ -1,1 +1,2 @@
 val encrypt : ?maxmem:int -> ?maxmemfrac:float -> ?maxtime:float -> string -> string -> string
+val decrypt : ?maxmem:int -> ?maxmemfrac:float -> ?maxtime:float -> string -> string -> string
